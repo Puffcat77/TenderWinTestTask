671ec001866257964c54a249d53fcbb8a175ccc7
Акопян Александр Айрапетович <akopia-sasha@mail.ru>
2021-12-09 17:14:40 +0300

    Добавьте файлы проекта.

---
+++ ConsoleVersion/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleVersion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter tender number:");
+            string tenderNumber = "";
+            
+        }
+    }
+}
